cd2318e017fa3dd70b4e37a59e3f3ac2c156ed09
Yuri Schulz - Zallpy <yurischulz@users.noreply.github.com>
2021-08-25 22:51:48 -0300

    created the auth route

--- src/components/Header/partials/SignInButton/index.tsx
+++ src/components/Header/partials/SignInButton/index.tsx
@@ -1,25 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 import { theme } from '../../../../styles/_colors';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color={theme.colors.green_500} />
-      Yuri Schulz
+      {session.user.name}
       <FiX color={theme.colors.gray_400} className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color={theme.colors.yellow_500} />
       Sign in with Github
